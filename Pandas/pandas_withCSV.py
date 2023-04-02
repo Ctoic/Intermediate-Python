ee276e3175feeb0d778b843166b31fe46695c0d9
ctoic <i202612@nu.edu.pk>
2023-04-02 17:58:58 +0500

    Added my exercise question from data camp

---
+++ Pandas/pandas_withCSV.py
@@ -0,0 +1,9 @@
+import pandas as pd 
+
+# importing the csv file
+
+df = pd.read_csv("C:/Users/Najam Ali Abbas/Desktop/Python/Pandas/CSV Files/Students.csv")
+
+# printing the dataframe
+
+print(df)
