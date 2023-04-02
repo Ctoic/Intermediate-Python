53975a578ea117d05760eb866ad27a3411d61982
ctoic <i202612@nu.edu.pk>
2023-04-03 03:54:53 +0500

    scatter plot was created for match data

--- eng2.py
+++ eng2.py
@@ -7,5 +7,21 @@ df = pd.read_csv('eng2.csv' , index_col = 0)
 
 print(df)
 
-# plotting given data on team names and goals scored 
- plt.plot(df['Team1'], df['FT'])
+# Storing Team 1 names in a numpy array from the dataframe
+team1 = df['Team 1'].values
+print(team1)
+
+# Now plotting them on a scatter plot 
+plt.scatter(x = team1 , y = df['Team 2'].values ,  c = 'red' , alpha = 0.8)
+
+# Previous customizations
+plt.xscale('log')
+
+plt.xlabel('Team 1')
+plt.ylabel('Team 2')
+plt.title('World Development in 2007')
+plt.xticks([1000,10000,100000], ['1k','10k','100k'])
+
+# Show the plot
+plt.show()
+
